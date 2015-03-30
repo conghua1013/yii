5390207730c828ea1a8e86109d9876314d285a43
mwq2020 <mwq2020@163.com>
2015-03-30 17:09:55 +0800

    aaa

--- Byguitar/views/layouts/main.php
+++ Byguitar/views/layouts/main.php
@@ -5,9 +5,9 @@
 	<meta name="language" content="en" />
 
 	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->hostInfo; ?>/css/screen.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->hostInfo; ?>/css/print.css" media="print" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->hostInfo; ?>/css/main.css" />
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
