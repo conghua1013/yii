d94fb99145ee0268985689aea4a7f8fad79d7f15
mwq2020 <mwq2020@163.com>
2015-03-30 14:23:12 +0800

    init

---
+++ Byguitar/index.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+/**
+ * This is the bootstrap file for test application.
+ * This file should be removed when the application is deployed for production.
+ */
+
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../framework/yii.php';
+$config=dirname(__FILE__).'/config/config.php';
+
+// remove the following line when in production mode
+// defined('YII_DEBUG') or define('YII_DEBUG',true);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
