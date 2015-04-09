98aaa8a12e9712ff0cce69d37a7d90e4a871508a
mwq2020 <mwq2020@163.com>
2015-04-09 18:21:02 +0800

    menu

---
+++ Byguitar/modules/manage/controllers/MenuController.php
@@ -0,0 +1,30 @@
+<?php
+
+class MenuController extends CController {
+
+	//列表页面
+	public function actionIndex(){
+
+	}
+
+	//添加页面
+	public function actionAdd(){
+
+	}
+
+	//编辑页面
+	public function actionEdit(){
+
+	}
+
+	//删除页面
+	public function actionDel(){
+
+	}
+
+	//树状图
+	public function actionTree(){
+
+	}
+
+}
