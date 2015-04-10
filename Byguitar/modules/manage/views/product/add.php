795ee089e068085b03e54b468347d401ae054329
mwq2020 <mwq2020@163.com>
2015-04-10 18:20:54 +0800

    test

--- Byguitar/modules/manage/views/product/add.php
+++ Byguitar/modules/manage/views/product/add.php
@@ -1,74 +1,46 @@
-<div class="pageContent">
-	<form method="post" action="demo/common/ajaxDone.html" class="pageForm required-validate" onsubmit="return validateCallback(this, navTabAjaxDone);">
-		<div class="pageFormContent" layoutH="56">
-			<div class="tabs" currentIndex="0" eventType="click">
-				<div class="tabsHeader">
-					<div class="tabsHeaderContent">
+<h2 class="contentTitle">编辑器</h2>
+<div style="display:block; overflow:hidden; padding:0 10px; line-height:21px;">
+	
+	<div class="tabs">
+		<div class="tabsHeader">
+			<div class="tabsHeaderContent">
+				<ul>
+					<li class="selected"><a href="javascript:;"><span>商品基本信息</span></a></li>
+					<li><a href="javascript:;"><span>商品详情</span></a></li>
+					<li><a href="javascript:;"><span>图片上传</span></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="tabsContent" layoutH="100">
+			<div>
+				<form method="post" action="demo/common/ajaxTimeout.html" class="pageForm required-validate" onsubmit="return iframeCallback(this)">
+					<div class="pageFormContent" layoutH="158">
+						<div class="unit">
+							<textarea class="editor" name="description" rows="40" cols="150"
+								upLinkUrl="upload.php" upLinkExt="zip,rar,txt" 
+								upImgUrl="upload.php" upImgExt="jpg,jpeg,gif,png" 
+								upFlashUrl="upload.php" upFlashExt="swf"
+								upMediaUrl="upload.php" upMediaExt:"avi">
+							</textarea>
+						</div>
+					</div>
+					<div class="formBar">
 						<ul>
-							<li><a href="javascript:;"><span>商品基本信息</span></a></li>
-							<li><a href="javascript:;"><span>商品库存信息</span></a></li>
-							<li><a href="javascript:;"><span>商品图片信息</span></a></li>
-							<!-- <li><a href="demo_page2.html" class="j-ajax"><span>标题3</span></a></li> -->
+							<li><div class="buttonActive"><div class="buttonContent"><button type="submit">提交</button></div></div></li>
+							<li><div class="button"><div class="buttonContent"><button type="button" class="close">取消</button></div></div></li>
 						</ul>
 					</div>
-				</div>
-				<div class="tabsContent" style="height:150px;">
-					<!-- 商品基本信息 start -->
-					<div>
-						<dl>
-							<dt>必填：</dt>
-							<dd>
-								<input type="text" name="name" maxlength="20" class="required" />
-								<span class="info">class="required"</span>
-							</dd>
-						</dl>
-						<dl>
-							<dt>必填：</dt>
-							<dd>
-								<input type="text" name="name" maxlength="20" class="required" />
-								<span class="info">class="required"</span>
-							</dd>
-						</dl>
-					</div>
-					<!-- 商品基本信息 end -->
-
-
-					<!-- 商品库存信息 start -->
-					<div>
-						<p>
-							<label>客 户 号：</label>
-							<input name="sn" type="text" size="30" value="100001" readonly="readonly"/>
-						</p>
-					</div>
-					<!-- 商品库存信息 end -->
-					
-
-					<!-- 商品图片信息 start -->
-					<div>
-						<dl>
-							<dt>必填：</dt>
-							<dd>
-								<input type="text" name="name" maxlength="20" class="required" />
-								<span class="info">class="required"</span>
-							</dd>
-						</dl>
-					</div>
-					<!-- 商品图片信息 end -->
-				</div>
-				<div class="tabsFooter">
-					<div class="tabsFooterContent"></div>
-				</div>
+				</form>
 			</div>
+			
+			<div>  </div>
 
+			<div>  </div>
+			
 		</div>
-		<div class="formBar">
-			<ul>
-				<li><div class="buttonActive"><div class="buttonContent"><button type="submit">保存</button></div></div></li>
-				<li>
-					<div class="button"><div class="buttonContent"><button type="button" class="close">取消</button></div></div>
-				</li>
-			</ul>
+		<div class="tabsFooter">
+			<div class="tabsFooterContent"></div>
 		</div>
-	</form>
+	</div>
+
 </div>
-
