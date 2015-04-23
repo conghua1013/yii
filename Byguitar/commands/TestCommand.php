5dea56143ee6d869db11725d3d9ac32867a3dc46
mwq2020 <mwq2020@163.com>
2015-04-23 13:52:51 +0800

    cron

--- Byguitar/commands/TestCommand.php
+++ Byguitar/commands/TestCommand.php
@@ -2,9 +2,6 @@
 
 class TestCommand extends CConsoleCommand  
 {  
-    //public function run($args) {  
-    //    echo "here";  
-    //}
     
     public function actionOrder(){
         echo "this is a order command cron";
