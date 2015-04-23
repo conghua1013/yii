b788e52e45221734dc590fc23cbd2f050db0078c
mwq <mwq@localhost.localdomain>
2015-04-23 13:51:05 +0800

    cron

--- Byguitar/commands/TestCommand.php
+++ Byguitar/commands/TestCommand.php
@@ -2,7 +2,11 @@
 
 class TestCommand extends CConsoleCommand  
 {  
-    public function run($args) {  
-        echo "here";  
+    //public function run($args) {  
+    //    echo "here";  
+    //}
+    
+    public function actionOrder(){
+        echo "this is a order command cron";
     }  
 }  
