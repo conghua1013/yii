ee9ca4ab691ce86cc4c229fda2b7d4df7dd5a698
mwq2020 <mwq2020@163.com>
2015-04-27 23:22:57 +0800

    test

--- Byguitar/config/cron.php
+++ Byguitar/config/cron.php
@@ -6,7 +6,7 @@
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'CMS Console',
+	'name'=>'Byguitar Console',
     // application components
     'components'=>array(
         //Main DB connection
